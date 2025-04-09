8f571f890040062326a26a04dc96b0f295d02055
BrandonFanaticProgramm <brandonalexisquintero.sena@gmail.com>
2025-04-08 23:37:03 -0500

    Agregando archivo inical para iniciar la construccion de la parte visual

---
+++ GESTOR/frontend/interface.py
@@ -0,0 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from backend.index import registrar_medicos
+import tkinter as tk
+from tkinter import messagebox
+
