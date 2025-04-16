16e4dc1e822ba610c42a235d384eac5ee43aae60
BrandonFanaticProgramm <brandonalexisquintero.sena@gmail.com>
2025-04-15 19:08:42 -0500

    Creando funciones recursivas para hacer el codigo mas legible sin repeticion

---
+++ GESTOR/backend/utils.py
@@ -0,0 +1,11 @@
+from typing import Optional #LIBRERIAS PARA EL TIPADO DEL CODIGO
+from index import  pacientes, medicos, enfermeros
+
+def buscar_paciente(paciente) -> Optional[dict]:
+    pass
+
+def buscar_medico(medico) -> Optional[dict]:
+    pass
+
+def buscar_enfermeros(enfermero) -> Optional[dict]:
+    pass
